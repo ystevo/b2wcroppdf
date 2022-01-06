c02d6d63d3ffa8f8b60c1880af8ae1f611647ea7
Dayanne Garcia <48609156+DayGarcia@users.noreply.github.com>
2022-01-06 17:09:25 -0300

    added magalu option

--- main.py
+++ main.py
@@ -10,6 +10,20 @@ folderSelected = ''
 current_index = 0
 numPages = 0
 
+#################################
+#               #               #
+#               #               #
+#  Upper left   #  Upper right  #
+#               #               #
+#               #               #
+#################################
+#               #               #
+#               #               #
+#  Lower right  #  Lower left   #
+#               #               #
+#               #               #
+#################################
+
 
 def file_open():
     global pdfSelected
@@ -23,7 +37,7 @@ def folder_open():
     window.txtFolder.setText(folderSelected)
 
 
-def pdf_slit():
+def b2w_split():
     with open(pdfSelected, "rb") as in_f:
         global current_index
         global numPages
@@ -60,15 +74,93 @@ def pdf_slit():
             page.scaleTo(4*72, 6*72)
             output.addPage(page)
 
-        with open(folderSelected+"\out.pdf", "wb") as out_f:
-            output.write(out_f)
+    pdf(output)
 
-        if platform.system() == 'Darwin':
-            subprocess.call(('open', folderSelected+"\out.pdf"))
-        elif platform.system() == 'Windows':
-            os.startfile(folderSelected+"\out.pdf")
-        else:
-            subprocess.call(('xdg-open', folderSelected+"\out.pdf"))
+
+def magalu_split():
+    with open(pdfSelected, "rb") as in_f:
+        global current_index
+        global numPages
+
+        input1 = PdfFileReader(in_f)
+        input2 = PdfFileReader(in_f)
+        input3 = PdfFileReader(in_f)
+        input4 = PdfFileReader(in_f)
+        output = PdfFileWriter()
+
+        numPages = input1.getNumPages() * 4 - 4
+        numPagesa = input1.getNumPages()
+        print("document has %s pages." % numPages)
+
+        # upper left
+        for i in range(numPagesa):
+            pages1 = input1.getPage(i)
+            if(i != 0):
+                current_index = i
+            progress_bar()
+            if(i == 0):  # first page is PLP so we ignore it
+                continue
+            pages1.mediaBox.lowerRight = (300, 420)
+
+            pages1.scaleTo(4*72, 6*72)
+            output.addPage(pages1)
+            print(i)
+
+        # upper right
+        for j in range(numPagesa):
+            pages2 = input2.getPage(j)
+            if(j != 0):
+                current_index = i+j
+            progress_bar()
+            if(j == 0):  # first page is PLP so we ignore it
+                continue
+            pages2.mediaBox.lowerRight = (600, 420)
+            pages2.mediaBox.upperLeft = (300, 840)
+
+            pages2.scaleTo(4*72, 6*72)
+            output.addPage(pages2)
+
+        # lower left
+        for k in range(numPagesa):
+            pages3 = input3.getPage(k)
+            if(j != 0):
+                current_index = i+j+k
+            progress_bar()
+            if(k == 0):  # first page is PLP so we ignore it
+                continue
+            pages3.mediaBox.lowerRight = (300, 1)
+            pages3.mediaBox.upperLeft = (1, 420)
+
+            pages3.scaleTo(4*72, 6*72)
+            output.addPage(pages3)
+
+        # lower right
+        for l in range(numPagesa):
+            pages4 = input4.getPage(l)
+            if(l != 0):
+                current_index = i+j+k+l
+            progress_bar()
+            if(l == 0):  # first page is PLP so we ignore it
+                continue
+            pages4.mediaBox.lowerRight = (600, 1)
+            pages4.mediaBox.upperLeft = (300, 420)
+
+            pages4.scaleTo(4*72, 6*72)
+            output.addPage(pages4)
+
+        pdf(output)
+
+
+def pdf(output):
+    with open(folderSelected+"\out.pdf", "wb") as out_f:
+        output.write(out_f)
+
+    if platform.system() == 'Darwin':
+        subprocess.call(('open', folderSelected+"\out.pdf"))
+    elif platform.system() == 'Windows':
+        os.startfile(folderSelected+"\out.pdf")
+    else:
+        subprocess.call(('xdg-open', folderSelected+"\out.pdf"))
 
 
 def progress_bar():
@@ -82,7 +174,8 @@ window = uic.loadUi("mainwin.ui")
 
 window.btnFile.clicked.connect(file_open)
 window.btnFolder.clicked.connect(folder_open)
-window.btnAction.clicked.connect(pdf_slit)
+window.btnActionB2W.clicked.connect(b2w_split)
+window.btnActionMagalu.clicked.connect(magalu_split)
 
 window.show()
 app.exec()
